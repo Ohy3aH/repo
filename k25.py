48463030254e70626a3b3d688f6b0a60f3a6ec0a
Alessander Arellano <105311842+Ohy3aH@users.noreply.github.com>
2022-09-13 19:31:09 +0000

    update

--- k25.py
+++ k25.py
@@ -2,11 +2,34 @@ from stanfordkarel import *
 
 
 def main():
-  """ karel code goes here """
-  move()
-  move()
-  move_left()
-  move()
-  move()
-  pick_beeper()
-  pass
+    """ karel code goes here """
+    move()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    pick_beeper()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    pass
+    
+  
+if __name__ == "__main__":
+    run_karel_program()
