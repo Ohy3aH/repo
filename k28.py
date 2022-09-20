7ea5d36c89a04dc3df72152cf7f66f5d7f5fe5de
Alessander Arellano <105311842+Ohy3aH@users.noreply.github.com>
2022-09-20 19:36:07 +0000

    update

---
+++ k28.py
@@ -0,0 +1,51 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    move()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    move()
+    put_beeper()
+    turn_left()
+    move()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    move()
+    move()
+    put_beeper()
+    turn_left()
+    turn_left()
+    turn_left()
+    turn_left()
+    turn_left()
+    turn_left()
+    turn_left()
+    run_karel_program()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
